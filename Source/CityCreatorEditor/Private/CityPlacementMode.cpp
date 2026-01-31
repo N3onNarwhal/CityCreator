a5bb08251853bd9702e4c9c2f35857dc04709a9a
Avery Lanier <averytlanier@icloud.com>
2026-01-31 13:44:59 -0600

    Added left click to viewport debug spheres

--- Source/CityCreatorEditor/Private/CityPlacementMode.cpp
+++ Source/CityCreatorEditor/Private/CityPlacementMode.cpp
@@ -1,22 +1,31 @@
+// CityPlacementMode.cpp
+
 #include "CityPlacementMode.h"
 
 #include "Editor.h"
 #include "EditorViewportClient.h"
 #include "Engine/World.h"
+#include "DrawDebugHelpers.h"
 
+
+// Unique ID for this editor mode
 const FEditorModeID FCityPlacementMode::EM_CityPlacement =
 TEXT("EM_CityPlacement");
 
 
+// Constructor
 FCityPlacementMode::FCityPlacementMode()
 {
+    UE_LOG(LogTemp, Warning, TEXT("CityPlacementMode Constructor"));
 }
 
+// Destructor
 FCityPlacementMode::~FCityPlacementMode()
 {
 }
 
 
+// Called when mode is activated
 void FCityPlacementMode::Enter()
 {
     FEdMode::Enter();
@@ -24,6 +33,8 @@ void FCityPlacementMode::Enter()
     UE_LOG(LogTemp, Warning, TEXT("CityPlacementMode Entered"));
 }
 
+
+// Called when mode is deactivated
 void FCityPlacementMode::Exit()
 {
     UE_LOG(LogTemp, Warning, TEXT("CityPlacementMode Exited"));
@@ -32,13 +43,81 @@ void FCityPlacementMode::Exit()
 }
 
 
+// Handle mouse clicks in viewport
 bool FCityPlacementMode::HandleClick(
     FEditorViewportClient* InViewportClient,
     HHitProxy* HitProxy,
     const FViewportClick& Click
 )
 {
-    UE_LOG(LogTemp, Warning, TEXT("Viewport Clicked"));
+    if (!InViewportClient)
+        return false;
+
+    UWorld* World = InViewportClient->GetWorld();
+    if (!World)
+        return false;
+
+
+    // Build scene view
+    FSceneViewFamilyContext ViewFamily(
+        FSceneViewFamily::ConstructionValues(
+            InViewportClient->Viewport,
+            InViewportClient->GetScene(),
+            InViewportClient->EngineShowFlags
+        ).SetRealtimeUpdate(true)
+    );
+
+    FSceneView* View = InViewportClient->CalcSceneView(&ViewFamily);
+
+    if (!View)
+        return false;
+
+
+    // Convert screen pos to ray
+    FVector RayOrigin;
+    FVector RayDirection;
+
+    View->DeprojectFVector2D(
+        Click.GetClickPos(),
+        RayOrigin,
+        RayDirection
+    );
+
+
+    FVector RayEnd = RayOrigin + RayDirection * 200000.f;
+
+    FHitResult Hit;
+
+
+    bool bHit = World->LineTraceSingleByChannel(
+        Hit,
+        RayOrigin,
+        RayEnd,
+        ECC_Visibility
+    );
+
+
+    if (bHit)
+    {
+        FVector HitPoint = Hit.ImpactPoint;
+
+        UE_LOG(
+            LogTemp,
+            Warning,
+            TEXT("Placement Hit: %s"),
+            *HitPoint.ToString()
+        );
+
+        DrawDebugSphere(
+            World,
+            HitPoint,
+            25.f,
+            12,
+            FColor::Green,
+            false,
+            2.f
+        );
+    }
 
     return true;
 }
